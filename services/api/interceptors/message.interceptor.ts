3ed5b3b0ecd9546a977157cae9bf089753e8fbb3
bwogt <bwogt.dev@gmail.com>
2026-06-13 16:36:10 -0300

    fix(axios): fixed connection error message

--- services/api/interceptors/message.interceptor.ts
+++ services/api/interceptors/message.interceptor.ts
@@ -18,7 +18,7 @@ export function responseInterceptor(response: AxiosResponse) {
 
 export function responseErrorInterceptor(error: AxiosError) {
   if (!error.response) {
-    notify({ type: 'error', text: i18n.t('errors:noConnection') });
+    notify({ type: 'error', text: i18n.t('errors:axios.noConnection') });
     return Promise.reject(error);
   }
 
