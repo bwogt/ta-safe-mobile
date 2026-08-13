bba0f996acb604878361a9638430466b633900d9
bwogt <bwogt.dev@gmail.com>
2026-08-13 13:55:57 -0300

    refactor(device-info): remove share and info buttons from device detail header
    The share functionality and info action are not currently used and will be
    added back in future dedicated activities.
    
    - Removed share button from DeviceInfo header
    - Removed info button with alert action from DeviceInfo header

--- components/ui/DeviceInfo/_header.tsx
+++ components/ui/DeviceInfo/_header.tsx
@@ -1,6 +1,5 @@
 import { Device } from '@/schemas/device/base/device.schema';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, View } from 'react-native';
 import StatusIndicator from '../StatusIndicator';
 
 type DeviceInfoHeaderProps = {
@@ -9,20 +8,10 @@ type DeviceInfoHeaderProps = {
 
 export default function DeviceInfoHeader({ device }: DeviceInfoHeaderProps) {
   return (
-    <View className="flex-row items-center justify-between px-4 pt-4">
+    <View className="flex-row items-center justify-start px-4 pt-4">
       <View>
         <Text className="text-xl  font-bold">{device.model.name}</Text>
         <StatusIndicator status={device.validation_status} />
-      </View>
-
-      <View className="flex-row items-center gap-4">
-        <TouchableOpacity>
-          <MaterialCommunityIcons name="information-outline" size={26} />
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <MaterialCommunityIcons name="share-variant-outline" size={22} />
-        </TouchableOpacity>
       </View>
     </View>
   );
